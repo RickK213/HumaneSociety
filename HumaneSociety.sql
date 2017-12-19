7ea5b46a2856f5a525e4fc873deffca8f3b1fa9e
RickK213 <rkippert@att.net>
2017-12-19 16:30:42 -0600

    Finished SQL commands for creating the database

---
+++ HumaneSociety.sql
@@ -0,0 +1,56 @@
+CREATE SCHEMA hs;
+
+CREATE TABLE hs.Species (
+	SpeciesID int IDENTITY(1,1) PRIMARY KEY,
+	SpeciesName varchar(50) NOT NULL,
+	SubSpeciesName varchar(50),
+	FoodPerWeek int	
+);
+
+CREATE TABLE hs.Animals (
+	AnimalID int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(50) NOT NULL,
+	SpeciesID int,
+	FOREIGN KEY (SpeciesID) REFERENCES hs.Species(SpeciesID),
+	RoomNumber int,
+	IsAdopted int,
+	HasShots int,
+	Price float
+);
+
+CREATE TABLE hs.Cities (
+	CityID int IDENTITY(1,1) PRIMARY KEY,
+	CityName varchar(50) NOT NULL	
+);
+
+CREATE TABLE hs.States (
+	StateID int IDENTITY(1,1) PRIMARY KEY,
+	StateName varchar(2) NOT NULL	
+);
+
+CREATE TABLE hs.Zip_Codes (
+	ZipCodeID int IDENTITY(1,1) PRIMARY KEY,
+	Number varchar(5) NOT NULL
+);
+
+CREATE TABLE hs.Addresses (
+	AddressID int IDENTITY(1,1) PRIMARY KEY,
+	Street1 varchar(100) NOT NULL,
+	Street2 varchar(100),
+	CityID int,
+	FOREIGN KEY (AddressID) REFERENCES hs.Cities(CityID),
+	StateID int,
+	FOREIGN KEY (StateID) REFERENCES hs.States(StateID),
+	ZipCodeID int,
+	FOREIGN KEY (ZipCodeID) REFERENCES hs.Zip_Codes(ZipCodeID)
+);
+
+CREATE TABLE hs.Adopters (
+	AdopterID int IDENTITY(1,1) PRIMARY KEY,
+	AdopterName varchar(50) NOT NULL,
+	AdopterEmail varchar(50) NOT NULL,
+	AdopterPassword varchar(50) NOT NULL,
+	AddressID int,
+	FOREIGN KEY (AddressID) REFERENCES hs.Addresses(AddressID),
+	AccountBalance float
+);
