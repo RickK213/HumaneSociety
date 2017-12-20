c10888fc04875be339a295284e75e527734a4d21
RickK213 <rkippert@att.net>
2017-12-20 09:00:22 -0600

    Added basic UI methods

--- HumaneSociety/HumaneSociety.cs
+++ HumaneSociety/HumaneSociety.cs
@@ -20,5 +20,21 @@ namespace HumaneSociety
             //create employee or adopter (create user)
             //display user's main menu
         }
+
+        public void Start_Rick()
+        {
+            UI.DisplayIntroScreen();
+            UI.DisplayMenuHeader();
+            userInput = UI.GetValidUserOption("1: Continue as an Adopter\n2: Continue as an Employee", new List<string>() { "1", "2" });
+            if ( userInput == "1" )
+            {
+                //create a new adopter
+            }
+            else
+            {
+                //create a new employee
+            }
+        }
+
     }
 }
