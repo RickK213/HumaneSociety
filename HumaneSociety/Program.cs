79f6b457b14922b79347b394ed82048a787cefd6
RickK213 <rkippert@att.net>
2017-12-21 14:48:55 -0600

    Fixed merge conflicts in Program class

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -10,15 +10,10 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            //HumaneSociety humaneSociety = new HumaneSociety();
-            //humaneSociety.Start();
-            ////test animal, remove later
-=======
             HumaneSociety humaneSociety = new HumaneSociety();
             humaneSociety.Start();
+            ////test animal, remove later
             //test animal, remove later
->>>>>>> 31199e65290a4a6967905d3801abfc0781cc0524
             //Animal animal = new Dog();
             //animal.Name = "Fido";
             //animal.IsAdopted = false;
@@ -26,15 +21,12 @@ namespace HumaneSociety
             //animal.OunceFoodPerWeek = 27;
             //animal.Price = 25.06;
             //animal.RoomNumber = 3;
-<<<<<<< HEAD
             ////-/remove
-            User adopter = new Adopter();
-            adopter.ListAnimals();
-=======
+            //User adopter = new Adopter();
+            //adopter.ListAnimals();
             //-/remove
             //DatabaseControl database = new DatabaseControl();
             //database.SearchAnimals(animal);
->>>>>>> 31199e65290a4a6967905d3801abfc0781cc0524
             Console.ReadKey();
         }
     }
