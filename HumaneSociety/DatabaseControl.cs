698a22cce2a6d1cb7e23bc95417bdba4a3523c33
Alex Waychoff <AlexWaychoff@gmail.com>
2017-12-21 10:56:02 -0600

    added search all method in DatabaseCommands.cs

--- HumaneSociety/DatabaseControl.cs
+++ HumaneSociety/DatabaseControl.cs
@@ -12,10 +12,11 @@ namespace HumaneSociety
     {
         public SqlConnection conn;
         public SqlTransaction transaction;
-        public SqlDataAdapter sda;
+        public SqlDataReader myDataReader = null;
+        public SqlDataAdapter databaseCommand;
         public SqlCommandBuilder scb;
-        public SqlCommand cmd;
-        DataTable dt;
+        public SqlCommand mySqlCommand;
+        DataTable fillerTable;
         
         string rickConnection = "Data Source=localhost;Initial Catalog=HumaneSociety;Integrated Security=True";
         string alexConnection = "Data Source=localhost;Initial Catalog = HumaneSociety; Integrated Security = True";
@@ -43,20 +44,81 @@ namespace HumaneSociety
             }
 
         }
-        public void SearchAnimals()
+        public void SearchAnimals(Animal animal)
         {
-            conn.Close();
-            conn.Open();
-            sda = new SqlDataAdapter("SELECT * FROM // ORDER BY score DESC", conn); //put table name to search from, specify search
-            dt = new DataTable();
-            sda.Fill(dt);
-            Console.WriteLine("TOP 5 SCORES");
-            Console.WriteLine("1. " + dt.Rows[0][1].ToString() + " played " + dt.Rows[0][2].ToString() + " days with a score of: " + dt.Rows[0][3].ToString());
-            Console.WriteLine("2. " + dt.Rows[1][1].ToString() + " played " + dt.Rows[1][2].ToString() + " days with a score of: " + dt.Rows[1][3].ToString());
-            Console.WriteLine("3. " + dt.Rows[2][1].ToString() + " played " + dt.Rows[2][2].ToString() + " days with a score of: " + dt.Rows[2][3].ToString());
-            Console.WriteLine("4. " + dt.Rows[3][1].ToString() + " played " + dt.Rows[3][2].ToString() + " days with a score of: " + dt.Rows[3][3].ToString());
-            Console.WriteLine("5. " + dt.Rows[4][1].ToString() + " played " + dt.Rows[4][2].ToString() + " days with a score of: " + dt.Rows[4][3].ToString());
+            try
+            {
+                SqlConnection mySqlConnection = new SqlConnection(connectionUsed);
+                mySqlCommand = new SqlCommand("SELECT * FROM hs.Animals ORDER BY SpeciesID DESC", mySqlConnection); //put table name to search from, specify search
+                mySqlConnection.Open();
+                myDataReader = mySqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+                //column order: name, species, room#, isAdopted, isImmunized, price, foodPerWeek
+                List<string> animalNames = new List<string>();
+                List<int> animalSpecies = new List<int>();
+                List<int> animalRoom = new List<int>();
+                List<bool> animalIsAdopted = new List<bool>();
+                List<bool> animalIsImmunized = new List<bool>();
+                List<double> animalPrice = new List<double>();
+                List<int> animalFood = new List<int>();
+                while (myDataReader.Read())
+                {
+                    animalNames.Add(myDataReader.GetString(1));
+                    animalSpecies.Add(myDataReader.GetInt32(2));
+                    animalRoom.Add(myDataReader.GetInt32(3));
+                    animalIsAdopted.Add(myDataReader.GetBoolean(4));
+                    animalIsImmunized.Add(myDataReader.GetBoolean(5));
+                    animalPrice.Add(myDataReader.GetDouble(6));
+                    animalFood.Add(myDataReader.GetInt32(7));
+                }
+                myDataReader.Close();
+                conn.Close();
+                Console.WriteLine("   | Name | Species | Room | Adoption Status | Immunization Status | Price | Food Per Week");
+                for(int i = 0; i < animalNames.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + ". {0}|{1}|{2}|{3}|{4}|{5}|{6}", animalNames[i], animalSpecies[i], animalRoom[i], animalIsAdopted[i], animalIsImmunized[i], animalPrice[i], animalFood[i]);
+                }
+            }
+            catch
+            {
+
+            }
         }
+        public void ScanList<T>(List<T> scannedList)
+        {
+
+        }
+        //public string GetLeaderboard()
+        //{
+
+        //    SqlDataReader myDataReader = null;
+
+        //    //SqlConnection mySqlConnection = new SqlConnection(connectionString);
+        //    mySqlCommand = new SqlCommand("SELECT TOP 5 Player_Name, Total_Profit FROM ls.High_Scores ORDER BY Total_Profit DESC;", mySqlConnection);
+        //    mySqlConnection.Open();
+        //    myDataReader = mySqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+        //    List<string> playerNames = new List<string>();
+        //    List<double> playerScores = new List<double>();
+        //    while (myDataReader.Read())
+        //    {
+        //        playerNames.Add(myDataReader.GetString(0));
+        //        playerScores.Add(myDataReader.GetDouble(1));
+        //    }
+        //    // Always call Close when done reading.
+        //    myDataReader.Close();
+        //    // Close the connection when done with it.
+        //    mySqlConnection.Close();
+
+        //    string leaderboard = "L E A D E R B O A R D\n";
+        //    leaderboard += "=====================\n\n";
+        //    for (int i = 0; i < playerNames.Count; i++)
+        //    {
+        //        leaderboard += (i + 1) + ": " + playerNames[i] + "\n   ";
+        //        leaderboard += playerScores[i].ToString("C2") + "\n\n";
+        //    }
+        //    return leaderboard;
+        //}
         public void AddAnimal(Animal animal)   //put arguments here to add full adoptable animal to database
         {
             string sqlQuery = "INSERT INTO hs.Animals VALUES(@Name, @SpeciesID, @RoomNumber, @IsAdopted, @HasShots, @Price, @FoodPerWeek);"; //put name of table here (dbo.HighScores) and change @'s to appropriate terms
@@ -65,17 +127,26 @@ namespace HumaneSociety
                 using (SqlCommand querySaveStaff = new SqlCommand(sqlQuery))
                 {
                     //name, species, room#, isAdopted, isImmunized, price, foodPerWeek
-                    openCon.Open();
-                    querySaveStaff.Connection = openCon;
-                    querySaveStaff.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = animal.Name;
-                    querySaveStaff.Parameters.Add("@SpeciesID", SqlDbType.Int).Value = animal.SpeciesID;
-                    querySaveStaff.Parameters.Add("@RoomNumber", SqlDbType.Int).Value = animal.RoomNumber;
-                    querySaveStaff.Parameters.Add("@IsAdopted", SqlDbType.Bit).Value = animal.IsAdopted;
-                    querySaveStaff.Parameters.Add("@HasShots", SqlDbType.Bit).Value = animal.IsImmunized;
-                    querySaveStaff.Parameters.Add("@Price", SqlDbType.Float).Value = animal.Price;
-                    querySaveStaff.Parameters.Add("@FoodPerWeek", SqlDbType.Int).Value = animal.OunceFoodPerWeek;
+                    try
+                    {
+                        //
+                        openCon.Open();
+                        querySaveStaff.Connection = openCon;
+                        querySaveStaff.Parameters.Add("@Name", SqlDbType.VarChar, 50).Value = animal.Name;
+                        querySaveStaff.Parameters.Add("@SpeciesID", SqlDbType.Int).Value = animal.SpeciesID;
+                        querySaveStaff.Parameters.Add("@RoomNumber", SqlDbType.Int).Value = animal.RoomNumber;
+                        querySaveStaff.Parameters.Add("@IsAdopted", SqlDbType.Bit).Value = animal.IsAdopted;
+                        querySaveStaff.Parameters.Add("@HasShots", SqlDbType.Bit).Value = animal.IsImmunized;
+                        querySaveStaff.Parameters.Add("@Price", SqlDbType.Float).Value = animal.Price;
+                        querySaveStaff.Parameters.Add("@FoodPerWeek", SqlDbType.Int).Value = animal.OunceFoodPerWeek;
 
-                    querySaveStaff.ExecuteNonQuery();
+                        querySaveStaff.ExecuteNonQuery();
+                        openCon.Close();
+                    }
+                    catch
+                    {
+                        //enter UI method that says not all information successfully passed through, try again
+                    }
 
                 }
             }
